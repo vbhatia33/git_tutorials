14b662136acf387c116da35e446e9dbea157669b
Vishesh <vbhatia33@gmail.com>
2023-01-31 14:02:21 +0530

    commiting other files also

---
+++ include/header_file.h
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void header_file_function(){
+    cout<<"This is header file"<<endl;
+}
---
+++ main_sample2.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include ".\include\header_file.h"
+using namespace std;
+int main(){
+    cout<<"This is the main file second"<<endl;
+    header_file_function();
+    return 0;
+}
